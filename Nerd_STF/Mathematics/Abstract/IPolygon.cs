2ca17a4a647df9e6c7f907dd8ed365b9ab98b49a
That_One_Nerd <dev@that-one-nerd.net>
2023-08-07 15:33:19 -0400

    Quickie. Some additions to the IPolygon interface.

---
+++ Nerd_STF/Mathematics/Abstract/IPolygon.cs
@@ -0,0 +1,7 @@
+﻿namespace Nerd_STF.Mathematics.Abstract;
+
+public interface IPolygon : ITriangulate
+{
+    public Float3[] GetAllVerts();
+    public float[] ToFloatArray();
+}
--- Nerd_STF/Mathematics/Abstract/ITriangulate.cs
+++ Nerd_STF/Mathematics/Abstract/ITriangulate.cs
@@ -2,13 +2,13 @@
 
 public interface ITriangulate
 {
-    public static Triangle[] TriangulateAll(params ITriangulate[] triangulatables)
+    public static virtual Triangle[] TriangulateAll(params ITriangulate[] triangulatables)
     {
         List<Triangle> res = new();
         foreach (ITriangulate triangulatable in triangulatables) res.AddRange(triangulatable.Triangulate());
         return res.ToArray();
     }
-    public static Triangle[] TriangulateAll<T>(params T[] triangulatables) where T : ITriangulate
+    public static virtual Triangle[] TriangulateAll<T>(params T[] triangulatables) where T : ITriangulate
     {
         List<Triangle> res = new();
         foreach (ITriangulate triangulatable in triangulatables) res.AddRange(triangulatable.Triangulate());
