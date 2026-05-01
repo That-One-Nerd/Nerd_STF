34681283c6a7b9c9657f6e9c2fe2786bc80f1163
That-One-Nerd <develop@thatonenerd.net>
2026-05-01 11:56:02 -0400

    The start of Polar3d.

---
+++ Nerd_STF/Mathematics/Numbers/Polar3d.cs
@@ -0,0 +1,35 @@
+﻿using Nerd_STF.Mathematics.Algebra;
+using System;
+
+namespace Nerd_STF.Mathematics.Numbers
+{
+    public struct Polar3d/* : IVector<Polar3d>, IEquatable<Polar3d>
+#if CS11_OR_GREATER
+                           ,IFromTuple<Polar3d, (double, Angle, Angle)>,
+                            IPresets3d<Polar3d>
+#endif*/
+    {
+        public double Magnitude { get; set; }
+        public Angle Theta { get; set; }
+        public Angle Phi { get; set; }
+
+        public Polar3d(double magnitude, Angle theta, Angle phi)
+        {
+            Theta = theta;
+            Phi = phi;
+            Magnitude = magnitude;
+        }
+        public Polar3d(double magnitude, double theta, double phi)
+        {
+            Magnitude = magnitude;
+            Theta = Angle.FromRadians(theta);
+            Phi = Angle.FromRadians(phi);
+        }
+        public Polar3d(Fill<double> fill)
+        {
+            Magnitude = fill(0);
+            Theta = Angle.FromRadians(fill(1));
+            Phi = Angle.FromRadians(fill(2));
+        }
+    }
+}
